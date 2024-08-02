65f92b810917c9062a604250e825d10e11cdb4c4
niharMohantyDev <mnihar198@.com>
2024-08-02 14:32:06 +0530

    initialised repo

---
+++ freshservice_integrationapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FreshserviceIntegrationappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'freshservice_integrationapp'
